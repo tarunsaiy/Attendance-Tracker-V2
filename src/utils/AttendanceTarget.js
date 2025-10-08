28eca99701af3fa20f9d916e07da3d6c3e355203
TARUN SAI YENDAVA <tarunsaiyendava@gmail.com>
2025-10-08 12:33:39 +0530

    changed num of skip periods

--- src/utils/AttendanceTarget.js
+++ src/utils/AttendanceTarget.js
@@ -2,11 +2,11 @@ function attendanceTarget(x, y) {
   const currentPercentage = (x / y) * 100;
 
   // 75% of total
-  const seventyFiveValue = 0.75 * y;
+
 
   // If below 75% → how much more needed
   if (currentPercentage < 75) {
-    const neededToReach = Math.ceil(seventyFiveValue - x);
+    const neededToReach = Math.ceil((0.75 * y - x) / 0.25);;
     return {
       currentPercentage: currentPercentage.toFixed(2) + "%",
       neededToReach,
@@ -16,7 +16,7 @@ function attendanceTarget(x, y) {
 
   // If above 75% → how much you can reduce and still stay at 75%
   if (currentPercentage > 75) {
-    const canDecrease = Math.floor(x - seventyFiveValue);
+    const canDecrease =  Math.floor((x - 0.75 * y) / 0.75);
     return {
       currentPercentage: currentPercentage.toFixed(2) + "%",
       neededToReach: 0,
